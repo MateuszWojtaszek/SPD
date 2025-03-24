9fc8504641225fb144341d1b50bdceb1859e6e31
mateuszwojtaszek <mateusz.wojtaszek@icloud.com>
2025-03-24 10:32:12 +0100

    na 3.5 mamy

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -36,53 +36,95 @@ struct Task {
     int release_time;
     int cooling_time;
 
-  std::string to_string(){
-    return "Task ID: " + std::to_string(id) + ", Processing Time: " + std::to_string(processing_time) +
-         ", Release Time: " + std::to_string(release_time) + ", Cooling Time: " + std::to_string(cooling_time);
-  }
+    std::string to_string() const{
+        return "Task ID: " + std::to_string(id) + ", Processing Time: " + std::to_string(processing_time) +
+               ", Release Time: " + std::to_string(release_time) + ", Cooling Time: " + std::to_string(cooling_time);
+    }
 };
 
-class ProblemN{
-
+class ProblemN {
 public:
-  ProblemN(std::vector<Task> tasks){
-    this->tasks = tasks;
-  }
+    ProblemN(std::vector<Task> tasks) {
+        this->tasks = tasks;
+    }
 
-  void generate_test_instance(std::vector<Task> &tasks) {
-      for (int i = 0; i < tasks.size(); i++) {
-          tasks[i].id = i + 1;
-          tasks[i].processing_time = rand() % 10 + 1; // Random processing time between 1 and 10
-          tasks[i].release_time = rand() % 5; // Random release time between 0 and 4
-          tasks[i].cooling_time = rand() % 5 + 1; // Random cooling time between 1 and 5
-      }
-  }
+    void generate_test_instance(std::vector<Task> &tasks) {
+        for (int i = 0; i < tasks.size(); i++) {
+            tasks[i].id = i + 1;
+            tasks[i].processing_time = rand() % 10 + 1; // Random processing time between 1 and 10
+            tasks[i].release_time = rand() % 5; // Random release time between 0 and 4
+            tasks[i].cooling_time = rand() % 5 + 1; // Random cooling time between 1 and 5
+        }
+    }
 
-  void print_task_instance() {
-      std::cout << "Task ID | Processing Time | Release Time | Cooling Time" << std::endl;
-      for (int i = 0; i < tasks.size(); i++) {
-          std::cout << tasks[i].to_string() << std::endl;
-      }
-  }
-  int calculate_Cmax() {
-      int Cmax = 0;
-      int current_time = 0;
+    void print_task_instance() {
+        std::cout << "Task ID | Processing Time | Release Time | Cooling Time" << std::endl;
+        for (int i = 0; i < tasks.size(); i++) {
+            std::cout << tasks[i].to_string() << std::endl;
+        }
+    }
 
-      for (int i = 0; i < tasks.size(); i++) {
-          if (current_time >= tasks[i].release_time) {
-              current_time = tasks[i].release_time;
-          }
-          current_time += tasks[i].processing_time;
-          Cmax = std::max(Cmax, current_time + tasks[i].cooling_time);
-      }
+    int calculate_Cmax() {
+        int Cmax = 0;
+        int current_time = 0;
 
-      return Cmax;
-  }
+        for (int i = 0; i < tasks.size(); i++) {
+            if (current_time >= tasks[i].release_time) {
+                current_time = tasks[i].release_time;
+            }
+            current_time += tasks[i].processing_time;
+            Cmax = std::max(Cmax, current_time + tasks[i].cooling_time);
+        }
 
-  private:
+        return Cmax;
+    }
+
+    void generate_permutations(std::vector<std::vector<Task> > &permutations) {
+        std::sort(tasks.begin(), tasks.end(), [](const Task &a, const Task &b) {
+            return a.id < b.id;
+        });
+
+        do {
+            permutations.push_back(tasks);
+        } while (std::next_permutation(tasks.begin(), tasks.end(), [](const Task &a, const Task &b) {
+            return a.id < b.id;
+        }));
+    }
+
+private
+:
     std::vector<Task> tasks;
 };
+void print_permutations(const std::vector<std::vector<Task>> &permutations) {
+    for (const auto &permutation : permutations) {
+        for (const auto &task : permutation) {
+            std::cout << task.to_string() << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+void calculate_Cmax_for_permutations(const std::vector<std::vector<Task>> &permutations) {
+    int min_Cmax = std::numeric_limits<int>::max();
+    std::vector<Task> best_permutation;
 
+    for (const auto &permutation : permutations) {
+        ProblemN problem(permutation);
+        int Cmax = problem.calculate_Cmax();
+        std::cout << "Cmax: " << Cmax << std::endl;
+        if (Cmax < min_Cmax) {
+            min_Cmax = Cmax;
+            best_permutation = permutation;
+        }
+    }
+
+    std::cout << "Minimum Cmax: " << min_Cmax << std::endl;
+    std::cout << "Best permutation:" << std::endl;
+    std::cout << "ID | Processing Time | Release Time | Cooling Time" << std::endl;
+    for (const auto &task : best_permutation) {
+        std::cout << task.id << "  | " << task.processing_time << "              | "
+                  << task.release_time << "            | " << task.cooling_time << std::endl;
+    }
+}
 int main() {
     // Task tasks[10];
     std::vector<Task> pregenerated_tasks = {
@@ -93,12 +135,17 @@ int main() {
         {5, 2, 6, 1}
     };
     ProblemN problem(pregenerated_tasks);
+    std::vector<std::vector<Task>> permutations = {};
+    problem.generate_permutations(permutations);
+    print_permutations(permutations);
+    std::cout << permutations.size()<<std::endl;
     // generate_test_instance(tasks, 10);
     // print_task_instance(tasks, 10);
     // int Cmax = calculate_Cmax(tasks, 10);
     problem.print_task_instance();
-    int Cmax_for_pregenerated = problem.calculate_Cmax();
-
-    std::cout << "Cmax for pregenerated: "<<Cmax_for_pregenerated << std::endl;
+    calculate_Cmax_for_permutations(permutations);
+    // int Cmax_for_pregenerated = problem.calculate_Cmax();
+    //
+    // std::cout << "Cmax for pregenerated: " << Cmax_for_pregenerated << std::endl;
     // std::cout << "Cmax: " << Cmax << std::endl;
 }
