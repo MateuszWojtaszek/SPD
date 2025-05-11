99f5161a31a02f8770d065ba0fba992af44bea49
Patryk Dudizński <dodpat02@gmail.com>
2025-05-12 00:13:44 +0200

    removed cmake0buikld dir

--- Problem2/src/PTAS.cpp
+++ Problem2/src/PTAS.cpp
@@ -17,5 +17,21 @@ PTAS::PTAS(std::vector<Task> tasks, int maszyny): BaseProblem(tasks, maszyny) {
 
 
 void PTAS::calculate_heuristic() {
-  // to do   
+  // to do
+
+  // Sort tasks in descending order of their processing times
+  std::sort(tasks.begin(), tasks.end(), [](const Task& a, const Task& b) {
+    return a.processing_time > b.processing_time;
+  });
+
+  // Distribute tasks among machines to minimize makespan
+  std::vector<int> machineLoad(maszyny, 0);
+  for (const auto& task : tasks) {
+    // Find the machine with the least load
+    auto minMachine = std::min_element(machineLoad.begin(), machineLoad.end());
+    auto minMachineIndex = std::distance(machineLoad.begin(), minMachine);
+    *minMachine += task.processing_time;
+    processors[minMachineIndex].tasks.push_back(task);
+  }
+    
 }
